b5c616b336dd5a720363ba2dcba4a7db45e2996c
Ben Floyd <ben.floyd@simplot.com>
2024-11-14 19:13:57 -0700

    Basic functionality

--- main.ts
+++ main.ts
@@ -1,12 +1,12 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
-
-// Remember to rename these classes and interfaces!
+import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, TFolder } from 'obsidian';
 
 interface FMISettings {
+	attachmentsFolder: string;
 	mySetting: string;
 }
 
 const DEFAULT_SETTINGS: FMISettings = {
+	attachmentsFolder: '',
 	mySetting: 'default'
 }
 
@@ -16,70 +16,19 @@ export default class FMI extends Plugin {
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
 		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+
+		this.addCommand({
+			id: 'find-broken-image-links',
+			name: 'Find Broken Image Links',
+			callback: () => {
+				this.findBrokenImageLinks();
+			}
+		});
 	}
 
 	onunload() {
-
 	}
 
 	async loadSettings() {
@@ -89,21 +38,60 @@ export default class FMI extends Plugin {
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
-}
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
+	private extractImageLinks(line: string): string[] {
+		const regex = /\[\[(.*?\.(jpg|jpeg|png|gif|bmp))(?:\|.*?)?\]\]/gi;
+		const matches: string[] = [];
+		let match;
+
+		while ((match = regex.exec(line)) !== null) {
+			matches.push(match[1]);
+		}
+		return matches;
 	}
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
+	private async imageExists(imagePath: string): Promise<boolean> {
+		return await this.app.vault.adapter.exists(imagePath);
 	}
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
+	private async findBrokenImageLinks(): Promise<void> {
+		let brokenLinksCount = 0;
+		const files = this.app.vault.getMarkdownFiles();
+		const attachmentsPath = this.settings.attachmentsFolder;
+
+		if (!attachmentsPath) {
+			new Notice('Please select an attachments folder in settings first!');
+			return;
+		}
+
+		for (const file of files) {
+			try {
+				const content = await this.app.vault.read(file);
+				const lines = content.split('\n');
+
+				lines.forEach((line, index) => {
+					const imageLinks = this.extractImageLinks(line);
+
+					imageLinks.forEach(async (imageFile) => {
+						let imagePath = imageFile;
+
+						if (!imagePath.includes('/')) {
+							imagePath = `${attachmentsPath}/${imageFile}`;
+						}
+
+						const exists = await this.imageExists(imagePath);
+						if (!exists) {
+							console.log(`Broken link found in file '${file.path}' at line ${index + 1}: ${imagePath}`);
+							brokenLinksCount++;
+						}
+					});
+				});
+			} catch (error) {
+				console.error(`Error processing file '${file.path}':`, error);
+			}
+		}
+
+		new Notice(`Total broken links found: ${brokenLinksCount}`);
 	}
 }
 
@@ -116,9 +104,28 @@ class SampleSettingTab extends PluginSettingTab {
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
+
+		new Setting(containerEl)
+			.setName('Attachments Folder')
+			.setDesc('Select the folder where your attachments are stored')
+			.addDropdown(dropdown => {
+				const folders = this.getFolders();
+				
+				dropdown.addOption('', '-- Select Folder --');
+				
+				folders.forEach(folder => {
+					dropdown.addOption(folder, folder);
+				});
+
+				dropdown.setValue(this.plugin.settings.attachmentsFolder)
+					.onChange(async (value) => {
+						this.plugin.settings.attachmentsFolder = value;
+						await this.plugin.saveSettings();
+					});
+			});
 
 		new Setting(containerEl)
 			.setName('Setting #1')
@@ -130,5 +137,19 @@ class SampleSettingTab extends PluginSettingTab {
 					this.plugin.settings.mySetting = value;
 					await this.plugin.saveSettings();
 				}));
+	}
+
+	private getFolders(): string[] {
+		const folders: string[] = [];
+		
+		const files = this.app.vault.getAllLoadedFiles();
+		
+		files.forEach(file => {
+			if (file instanceof TFolder) {
+				folders.push(file.path);
+			}
+		});
+		
+		return folders.sort();
 	}
 }
