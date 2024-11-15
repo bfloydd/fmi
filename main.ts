9b99f1307860121b0f99f50a127bfd938f516dd5
Ben Floyd <ben.floyd@simplot.com>
2024-11-14 20:44:00 -0700

    Remove unused

--- main.ts
+++ main.ts
@@ -134,17 +134,6 @@ class SampleSettingTab extends PluginSettingTab {
 						await this.plugin.saveSettings();
 					});
 			});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
 	}
 
 	private getFolders(): string[] {
